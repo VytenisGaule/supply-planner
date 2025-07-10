700f8556a42b03c47e6dc7d08366c86782b95117
Vytenis <vytenis.gaule@gmail.com>
2025-07-11 01:45:09 +0300

    Add Django REST framework integration and update URL routing

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from app.views import static_views
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', static_views.homepage, name='homepage'),
 
 ]
+
+urlpatterns.append(path('api/', include(router.urls)))
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Third party apps
+    'rest_framework',
     'django_extensions',
     
     # Local apps
@@ -179,4 +180,16 @@ LOGGING = {
             'propagate': True,
         },
     },
+}
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
