f63cdf3e4f29e23ddb6a3b0a5c659a82333b9ed7
Vytenis <vytenis.gaule@gmail.com>
2025-07-16 00:34:53 +0300

    Add pagination to product list and homepage views with responsive design

--- app/views/static_views.py
+++ app/views/static_views.py
@@ -2,11 +2,19 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import QuerySet
+from django.core.paginator import Paginator
 from app.models import Product
 
 @csrf_protect
 def homepage(request):
     context: dict = {}
-    products: QuerySet = Product.objects.all()
-    context['products'] = products
+    products: QuerySet = Product.objects.select_related('category').prefetch_related('suppliers').all()
+    
+    # Pagination
+    paginator = Paginator(products, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context['products'] = page_obj
+    context['paginator'] = paginator
     return render(request, 'pages/index.html', context=context)
